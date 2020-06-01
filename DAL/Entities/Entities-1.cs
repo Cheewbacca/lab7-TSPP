fdad7e9a1667d3494ede3a04d6c722b92fd3fdd3
Ivan <vanya.timoschenko85@gmail.com>
2020-06-02 00:15:19 +0300

    Added Entities to DAL

---
+++ DAL/Entities/Entities-1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalog.DAL.Entities
+{
+    public class OSBB
+    {
+        public int OSBBID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Director { get; set; }
+        public IEnumerable<Street> Streets { get; set; }
+        public IEnumerable<User> Users { get; set; }
+    }
+}
---
+++ DAL/Entities/Entities-2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalog.DAL.Entities
+{
+    public class Street
+    {
+        public int StreetId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
