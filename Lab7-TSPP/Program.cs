dd40f1065b14fef25b812b2e4b9bf065bee0138e
Ivan <vanya.timoschenko85@gmail.com>
2020-06-02 00:07:13 +0300

    Added DAL project

---
+++ Lab7-TSPP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab7_TSPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
